fe18c3dd86d7be69e267002d385f898c5fe3f906
Yash Agarwal <yagarwa2@ncsu.edu>
2023-11-03 14:17:46 -0400

    [Debug] Cleared errors

--- CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/models/Staff.java
+++ CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/models/Staff.java
@@ -2,34 +2,34 @@ package edu.ncsu.csc.CoffeeMaker.models;
 
 public class Staff extends User {
 
-	public ArrayList<Order> orders;
-
+//	public ArrayList<Order> orders;
+//
 	public Staff(long id, String userName, String passwordHash) {
 		super(id, userName, passwordHash);
 
 	}
-
-	/**
-	 * @return the orders
-	 */
-	public ArrayList<Order> getOrders() {
-		return orders;
-	}
-
-	/**
-	 * @param orders the orders to set
-	 */
-	public void setOrders(ArrayList<Order> orders) {
-		this.orders = orders;
-	}
-
-	private boolean checkInventory(Order order) {
-		//getting each recipe
-		for(order.getRecipes() entry: recipes.entrySet()) {
-			//getting ingredients of each recipe
-			entry.getKey().getIngredients
-		}
-		return false;
-	}
+//
+//	/**
+//	 * @return the orders
+//	 */
+//	public ArrayList<Order> getOrders() {
+//		return orders;
+//	}
+//
+//	/**
+//	 * @param orders the orders to set
+//	 */
+//	public void setOrders(ArrayList<Order> orders) {
+//		this.orders = orders;
+//	}
+//
+//	private boolean checkInventory(Order order) {
+//		//getting each recipe
+//		for(order.getRecipes() entry: recipes.entrySet()) {
+//			//getting ingredients of each recipe
+//			entry.getKey().getIngredients
+//		}
+//		return false;
+//	}
 
 }
