5b718b1caadab2bfab2e66132541eea8a4ab7e1d
Yash Agarwal <yagarwa2@ncsu.edu>
2023-11-28 22:19:33 -0500

    [Debug] Final conflict resolution

--- CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/config/WebSecurityConfig.java
+++ CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/config/WebSecurityConfig.java
@@ -15,61 +15,58 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserDetailsService userDetailsService;
+	@Autowired
+	private UserDetailsService userDetailsService;
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder () {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    /**
-     * Defines which URL paths should be secured and which should not
-     */
-    @Override
-    protected void configure ( final HttpSecurity http ) throws Exception {
-        http.authorizeRequests().antMatchers( "/", "/index", "/registry.html", "/api/v1/users/**", "/privacy.html" ).permitAll()
-                .antMatchers( "/manager/**" ).hasAuthority( "MANAGER" ).antMatchers( "/customer/**" )
-                .hasAuthority( "CUSTOMER" ).antMatchers( "/staff/**" ).hasAuthority( "STAFF" ).and().formLogin()
-                .loginPage( "/login" ).successHandler( successHandler() ).permitAll().and().logout()
-                .logoutSuccessUrl( "/" ).permitAll().and().csrf().disable();
-        http.authorizeRequests().anyRequest().authenticated();
-    }
+	/**
+	 * Defines which URL paths should be secured and which should not
+	 */
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/index", "/registry.html", "/api/v1/users/**", "/privacy.html")
+				.permitAll().antMatchers("/manager/**").hasAuthority("MANAGER").antMatchers("/customer/**")
+				.hasAuthority("CUSTOMER").antMatchers("/staff/**").hasAuthority("STAFF").and().formLogin()
+				.loginPage("/login").successHandler(successHandler()).permitAll().and().logout().logoutSuccessUrl("/")
+				.permitAll().and().csrf().disable();
+		http.authorizeRequests().anyRequest().authenticated();
+	}
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder () {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Autowired
-    public void configureAuth ( final AuthenticationManagerBuilder auth ) throws Exception {
-        auth.userDetailsService( userDetailsService ).passwordEncoder( bCryptPasswordEncoder() );
-    }
+	@Autowired
+	public void configureAuth(final AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
 
-    @Bean
-    public AuthenticationSuccessHandler successHandler () {
-        final SimpleUrlAuthenticationSuccessHandler handler = new SimpleUrlAuthenticationSuccessHandler();
-        handler.setDefaultTargetUrl( "/" ); // Set the default target URL after
-                                            // successful login
+	@Bean
+	public AuthenticationSuccessHandler successHandler() {
+		final SimpleUrlAuthenticationSuccessHandler handler = new SimpleUrlAuthenticationSuccessHandler();
+		handler.setDefaultTargetUrl("/"); // Set the default target URL after
+											// successful login
 
-        return ( request, response, authentication ) -> {
-            // Custom logic to determine the target URL based on user roles
-            if ( authentication.getAuthorities().stream()
-                    .anyMatch( grantedAuthority -> grantedAuthority.getAuthority().equals( "CUSTOMER" ) ) ) {
-                response.sendRedirect( "/customer/home" );
-            }
-            else if ( authentication.getAuthorities().stream()
-                    .anyMatch( grantedAuthority -> grantedAuthority.getAuthority().equals( "MANAGER" ) ) ) {
-                response.sendRedirect( "/manager/home" );
-            }
-            else if ( authentication.getAuthorities().stream()
-                    .anyMatch( grantedAuthority -> grantedAuthority.getAuthority().equals( "STAFF" ) ) ) {
-                response.sendRedirect( "/staff/home" );
-            }
-            else {
-                handler.onAuthenticationSuccess( request, response, authentication );
-            }
-        };
+		return (request, response, authentication) -> {
+			// Custom logic to determine the target URL based on user roles
+			if (authentication.getAuthorities().stream()
+					.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("CUSTOMER"))) {
+				response.sendRedirect("/customer/home");
+			} else if (authentication.getAuthorities().stream()
+					.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("MANAGER"))) {
+				response.sendRedirect("/manager/home");
+			} else if (authentication.getAuthorities().stream()
+					.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("STAFF"))) {
+				response.sendRedirect("/staff/home");
+			} else {
+				handler.onAuthenticationSuccess(request, response, authentication);
+			}
+		};
 
-    }
+	}
 }
