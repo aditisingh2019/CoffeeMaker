1ed825b7ce0bd0a21921fc2d5cd82ca22f9d8205
pbairol <pbairol@ncsu.edu>
2023-11-29 16:13:47 -0500

    Checkstyle Fixes throughout the development branch

--- CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/config/WebSecurityConfig.java
+++ CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/config/WebSecurityConfig.java
@@ -12,18 +12,32 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
+/**
+ * This is the configuration this helps us make sure only certain permission
+ * level roels can access certain cites. Once the User is authencated this class
+ * helps us map them to the right pages as well
+ */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private UserDetailsService userDetailsService;
+    /**
+     * This UserDetailsService class helps us load users and see if they can be
+     * authenticated by what is in the backend via username and password
+     */
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    /**
+     * This is a creation of a passwordenocder that other methods uses
+     *
+     * @return BCryptPasswordEncoder the type of password encoder
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
 
-	/**
+    /**
      * Defines which URL paths should be secured and which should not
      */
     @Override
@@ -36,37 +50,62 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().anyRequest().authenticated();
     }
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    /**
+     * This is a creation of a passwordenocder that other methods uses
+     *
+     * @return BCryptPasswordEncoder the type of password encoder
+     */
 
-	@Autowired
-	public void configureAuth(final AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-	}
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
 
-	@Bean
-	public AuthenticationSuccessHandler successHandler() {
-		final SimpleUrlAuthenticationSuccessHandler handler = new SimpleUrlAuthenticationSuccessHandler();
-		handler.setDefaultTargetUrl("/"); // Set the default target URL after
-											// successful login
+    /**
+     * This configures the auth and checks if the user can be logged in or not
+     *
+     * @param auth
+     *            the AuthenticationManagerBuilder object that we are trying to
+     *            see will return the authenticated user or not
+     * @throws Exception
+     *             Exception is thrown if the auth object runs into a problem
+     */
+    @Autowired
+    public void configureAuth ( final AuthenticationManagerBuilder auth ) throws Exception {
+        auth.userDetailsService( userDetailsService ).passwordEncoder( bCryptPasswordEncoder() );
+    }
 
-		return (request, response, authentication) -> {
-			// Custom logic to determine the target URL based on user roles
-			if (authentication.getAuthorities().stream()
-					.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("CUSTOMER"))) {
-				response.sendRedirect("/customer/home");
-			} else if (authentication.getAuthorities().stream()
-					.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("MANAGER"))) {
-				response.sendRedirect("/manager/home");
-			} else if (authentication.getAuthorities().stream()
-					.anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("STAFF"))) {
-				response.sendRedirect("/staff/home");
-			} else {
-				handler.onAuthenticationSuccess(request, response, authentication);
-			}
-		};
+    /**
+     * This is the success handler ojbect after the user is authenticated it
+     * remaps the user to the right site.
+     *
+     * @return the AuthenticationSuccessHandler which has a redirection link to
+     *         the right html page
+     */
+    @Bean
+    public AuthenticationSuccessHandler successHandler () {
+        final SimpleUrlAuthenticationSuccessHandler handler = new SimpleUrlAuthenticationSuccessHandler();
+        handler.setDefaultTargetUrl( "/" ); // Set the default target URL after
+                                            // successful login
 
-	}
+        return ( request, response, authentication ) -> {
+            // Custom logic to determine the target URL based on user roles
+            if ( authentication.getAuthorities().stream()
+                    .anyMatch( grantedAuthority -> grantedAuthority.getAuthority().equals( "CUSTOMER" ) ) ) {
+                response.sendRedirect( "/customer/home" );
+            }
+            else if ( authentication.getAuthorities().stream()
+                    .anyMatch( grantedAuthority -> grantedAuthority.getAuthority().equals( "MANAGER" ) ) ) {
+                response.sendRedirect( "/manager/home" );
+            }
+            else if ( authentication.getAuthorities().stream()
+                    .anyMatch( grantedAuthority -> grantedAuthority.getAuthority().equals( "STAFF" ) ) ) {
+                response.sendRedirect( "/staff/home" );
+            }
+            else {
+                handler.onAuthenticationSuccess( request, response, authentication );
+            }
+        };
+
+    }
 }
